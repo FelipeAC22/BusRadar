6348a827bc20e6576ffe899eda2b2a5c174098e2
felipe <felipeac2204@gmail.com>
2025-12-24 14:26:00 -0300

    UPDATE(_layout) os erros de semantica que impediam a função Layout  a utilizar os imports foram corrigidos

--- BusRadar/src/app/_layout.tsx
+++ BusRadar/src/app/_layout.tsx
@@ -1,22 +1,28 @@
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Stack } from "expo-router";
-import { StatusBar } from 'expo-status-bar'
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from "react-native";
 import { colors } from "../theme/colors";
 
 export default function layout() {
     return (
-        <GestureHandlerRootView style={Styles.container}>
-            <statusbar style="dark" />
+        <GestureHandlerRootView style={styles.container}>
+            <StatusBar style="dark" />
+            <Stack
+                screenOptions={{
+                    headerShown: false,
+                    contentStyle: { backgroundColor: colors.backgroud }
+                }}
 
-
-
-
-
-
-
+            >
+                <Stack.Screen name="index" />
+            </Stack>
         </GestureHandlerRootView>
-
-
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    }
+})
