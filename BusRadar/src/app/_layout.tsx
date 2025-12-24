edefdb4ae3edc16226cfa8f784bde9a98d3de2ed
felipe <felipeac2204@gmail.com>
2025-12-24 00:02:44 -0300

    arquivos iniciais do projeto foram alterados, por enquanto é isso

--- BusRadar/src/app/_layout.tsx
+++ BusRadar/src/app/_layout.tsx
@@ -1,5 +1,22 @@
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Stack } from "expo-router";
-import { StatusBar} from 'expo-status-bar'
+import { StatusBar } from 'expo-status-bar'
 import { StyleSheet } from "react-native";
 import { colors } from "../theme/colors";
+
+export default function layout() {
+    return (
+        <GestureHandlerRootView style={Styles.container}>
+            <statusbar style="dark" />
+
+
+
+
+
+
+
+        </GestureHandlerRootView>
+
+
+    )
+}
