3a9702218c84d5a0a48768c76a95a953233ecdfb
felipe <felipeac2204@gmail.com>
2025-12-26 21:29:03 -0300

    UPDATE(BottominfoPanel) adicionei mais código, mas ainda tá incompleto, ainda não podemos dar continuidade no código com a integração do mesmo

--- BusRadar/src/components/BottomInfoPanel.tsx
+++ BusRadar/src/components/BottomInfoPanel.tsx
@@ -12,12 +12,40 @@ import { colors } from "../theme/colors"; // imports
 const { height: SCREEN_HEIGHT } = Dimensions.get('window')
 const MAX_TRANSLATE_Y = -SCREEN_HEIGHT + 50 // definição de dimensão
 
-interface BottomInfoPanelProps { // loco kkk
+interface BottomInfoPanelProps { // precisa implementar
     isVisible: boolean
     etaMinutes: number
+    busLine: string
+    destination: string
+    targetLocationName?: string
+    status?: string
+    onNotifyPress?: () => void
 }
 
-export default function BottomInfoPanel({ isVisible, etaMinutes }: BottomInfoPanelProps) {
+// a constante primária pra função principal logo abaixo
+const styles = StyleSheet.create({ 
+    bottomSheet: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        bottom: -200,
+        height: SCREEN_HEIGHT,
+        backgroundColor: colors.card,
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+    }
+})
+
+
+export default function BottomInfoPanel({
+    isVisible,
+    etaMinutes,
+    busLine,
+    destination,
+    targetLocationName = "Your Location",
+    status = "On Time | Low \Croeding",
+    onNotifyPress
+}: BottomInfoPanelProps) {
     const translateY = useSharedValue(0)
     const context = useSharedValue({ y: 0 })
 
@@ -30,6 +58,36 @@ export default function BottomInfoPanel({ isVisible, etaMinutes }: BottomInfoPan
     })
     const gesture = Gesture.Pan()
         .onStart(() => {
-            context.value = {y: translateY.value} // deu bug muehehehe
+            context.value = { y: translateY.value }
         })
+        .onUpdate((event) => {
+            translateY.value = event.translationY + context.value.y
+        })
+        .onEnd(() => {
+            if (translateY.value > 50) {
+                translateY.value = withSpring(0)
+                // eu realmente preciso disso?
+                /* pelo jeito sim, e talvez tenho que colocar um onClose 
+                pra reiniciar se puxar muito longe da tela */
+            } else {
+                translateY.value = withSpring(0)
+            }
+        })
+
+    /* if (!isVisible) return null
+       temporariamente desativado para retornoo de animação 0 (escondido)
+       e conflitos para animações fora da tela */
+
+    // o maior return que eu ja vikkkk, retorna as animações
+    return (
+        <GestureDetector gesture={gesture}>
+            <Animated.View style={[styles.bottomSheet, animatedStyle]}>
+                <View style={styles.handle} />
+                <View style>
+                </View>
+            </Animated.View>
+        </GestureDetector>
+
+
+    )
 }
